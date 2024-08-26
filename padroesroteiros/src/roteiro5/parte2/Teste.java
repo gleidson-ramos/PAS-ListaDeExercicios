e564bf3853b2bebb6d133c05e928275e416c4b3c
Gleidson Ramos <130403942+gleidson-ramos@users.noreply.github.com>
2024-08-26 17:21:09 -0300

    parte 05
    singleton

---
+++ padroesroteiros/src/roteiro5/parte2/Teste.java
@@ -0,0 +1,17 @@
+package roteiro5.parte2;
+
+public class Teste{
+    public static void main(String[] args) {
+         
+        ControladorAereo c1 = new ControladorAereo();
+        ControladorAereo c2 = new ControladorAereo();
+        
+        c1.solicitarDecolagem();
+        c2.solicitarDecolagem();
+        
+        System.out.println(" ");
+        
+        c1.solicitarAterrissagem();
+        c2.solicitarAterrissagem();
+    }
+}
